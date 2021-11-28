d27dc04ad5fb971a25a6257528e746d8a50f0e03
Luis Peñaranda <luis.penaranda@gmx.com>
2021-11-28 16:53:30 -0300

    Refactor main cpp file.

--- Makefile
+++ Makefile
@@ -2,38 +2,41 @@
 #
 # This file is part of empdfer.
 #
-# Paddlefish is free software: you can redistribute it and/or modify
+# Empdfer is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Paddlefish is distributed in the hope that it will be useful,
+# Empdfer is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with paddlefish.  If not, see <http://www.gnu.org/licenses/>.
+# along with empdfer.  If not, see <http://www.gnu.org/licenses/>.
 
 # Do NOT use -ansi in CXXPARAMS when using valgrind. It is also better
 # to use -O0 in OPTIMIZATION in this case.
 
 PDF_LIB=paddlefish
 PDF_LIB_PATH=../../paddlefish/build
+PDF_LIB_INCLUDE_PATH=../../paddlefish/build
 EXT_LIBS_DEFS=-DPADDLEFISH_USE_ZLIB
 CXXPARAMS=-g ${EXT_LIBS_DEFS} -Wall -pedantic -std=c++11 #-ansi
 OPTIMIZATION=-O2
 EXT_LIBS=-lm -lz
 
-BINARIES=empdfer
+BINARY=empdfer
 
-%: %.cpp
-	g++ ${CXXPARAMS} ${OPTIMIZATION} $< -I${PDF_LIB_PATH} -L${PDF_LIB_PATH} -l${PDF_LIB} ${EXT_LIBS} -o $@
+OBJECTS=create_page.o empdfer.o
 
-all: ${PDF_LIB_PATH}/lib${PDF_LIB}.a ${BINARIES}
+%.o: %.cpp
+	g++ ${CXXPARAMS} ${OPTIMIZATION} -I${PDF_LIB_INCLUDE_PATH} -c $< -o $@
 
-empdfer: empdfer.cpp
-	g++ ${CXXPARAMS} ${OPTIMIZATION} $< -I${PDF_LIB_PATH} -L${PDF_LIB_PATH} -l${PDF_LIB} ${EXT_LIBS} -ljpeg -o $@
+all: ${PDF_LIB_PATH}/lib${PDF_LIB}.a ${BINARY}
+
+empdfer: ${OBJECTS}
+	g++ ${CXXPARAMS} ${OPTIMIZATION} -L${PDF_LIB_PATH} ${OBJECTS} -l${PDF_LIB} ${EXT_LIBS} -ljpeg -o $@
 
 clean:
-	rm -f *.o ${BINARIES} ${LIBNAME}
+	rm -f *.o ${BINARY}
---
+++ create_page.cpp
@@ -0,0 +1,102 @@
+// Copyright (c) 2021 Luis Peñaranda. All rights reserved.
+//
+// This file is part of empdfer.
+//
+// Empdfer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Empdfer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with empdfer.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "create_page.h"
+
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <jerror.h>
+#include <jpeglib.h>
+#include <sstream>
+
+paddlefish::PagePtr create_page(const std::string& input_file, double page_x_mm, double page_y_mm, double img_x_mm,
+                                double img_y_mm)
+{
+  paddlefish::PagePtr p(new paddlefish::Page());
+
+  // Compute image size using libjpeg. we need libjpeg only for this, because
+  // the image stream will be copied as-is by Paddlefish.
+  jpeg_decompress_struct cinfo;
+  struct jpeg_error_mgr err;
+
+  jpeg_create_decompress(&cinfo);
+  cinfo.err = jpeg_std_error(&err);
+
+  FILE* infile;
+  if ((infile = fopen(input_file.c_str(), "rb")) == NULL)
+  {
+    std::cerr << "can't open input file \"" << input_file << "\"" << std::endl;
+
+    exit(-1);
+  }
+
+  jpeg_stdio_src(&cinfo, infile);
+  jpeg_read_header(&cinfo, 0);
+  fclose(infile);
+
+  // Done with libjpeg.
+
+  // Compute the image size based on the resolution in the jpg.
+  if (img_x_mm == -1. && img_y_mm == -1.)
+  {
+    // Assume density is specified in DPI. Convert it to dots per mm.
+    double x_density_dpmm = (double)cinfo.X_density / 25.4;
+    double y_density_dpmm = (double)cinfo.Y_density / 25.4;
+
+    img_x_mm = cinfo.image_width / x_density_dpmm;
+    img_y_mm = cinfo.image_height / y_density_dpmm;
+  }
+  // Compute the missing dimensions to maintain aspect ratio in case one size
+  // was not specified.
+  else if (img_x_mm == -1.)
+    img_x_mm = (double)cinfo.image_width * img_y_mm / cinfo.image_height;
+  else
+    img_y_mm = (double)cinfo.image_height * img_x_mm / cinfo.image_width;
+
+  // Compute the margins needed to center the image on page.
+  double margin_x_mm = (page_x_mm - img_x_mm) / 2.;
+  double margin_y_mm = (page_y_mm - img_y_mm) / 2.;
+
+  // Set the page size.
+  p->set_mediabox(0, 0, MILIMETERS(page_x_mm), MILIMETERS(page_y_mm));
+
+  // TODO: remove showing this debug information.
+  std::cerr << "jpeg_color_space=" << cinfo.jpeg_color_space <<
+    ", out_color_space=" << cinfo.out_color_space << std::endl;
+
+  // Add the jpeg image to the page. For this, try find which color space
+  // the image is in.
+  switch (cinfo.jpeg_color_space)
+  {
+    case JCS_GRAYSCALE:
+      p->add_jpeg_image(input_file,
+                        cinfo.image_width, cinfo.image_height,
+                        MILIMETERS(margin_x_mm), MILIMETERS(margin_y_mm), MILIMETERS(img_x_mm), MILIMETERS(img_y_mm),
+                        COLORSPACE_DEVICEGRAY);
+      break;
+
+    default:
+      p->add_jpeg_image(input_file,
+                        cinfo.image_width, cinfo.image_height,
+                        MILIMETERS(margin_x_mm), MILIMETERS(margin_y_mm), MILIMETERS(img_x_mm), MILIMETERS(img_y_mm),
+                        COLORSPACE_DEVICERGB);
+      break;
+  }
+
+  return p;
+}
